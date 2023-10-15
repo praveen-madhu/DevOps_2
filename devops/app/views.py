dc96162c28dbdab28fa30251956d1f47e3ac8282
praveen-madhu <praveenmadhu2004@gmail.com>
2023-10-15 14:28:15 +0530

    first commit

---
+++ devops/app/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+import requests
+
+api_key = "733057631a05433282178985372db301"
+
+# Create your views here.
+
+def home(request):
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={api_key}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={api_key}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+
+
+
+    context = {
+        'articles' : articles
+    }
+
+    return render(request, 'app/templates/home.html', context)
